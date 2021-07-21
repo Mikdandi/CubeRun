03bce3f69e949561072eabb50a30a268f8c5a14f
Misha <mishakryvoshea@gmail.com>
2021-07-21 10:36:40 +0300

    first commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Movement movement;
+     void OnCollisionEnter (Collision collisionInfo)
+    {
+        if(collisionInfo.collider.CompareTag("Obstacle"))
+        {
+            Debug.Log("We hit an Obstacle");
+        }
+       
+
+    }
+}
